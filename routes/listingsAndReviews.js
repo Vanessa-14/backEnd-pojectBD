3628ebef45956d75ae23f08d51097abea7370e7f
María Vanessa <mariaramirez1433@gmail.com>
2020-03-19 15:35:41 -0600

    propiedades y rango de precio listos

--- routes/listingsAndReviews.js
+++ routes/listingsAndReviews.js
@@ -19,6 +19,41 @@ app.get('/listingsAndReview', (req, res) => {
         });
 });
 
+app.get('/listingsAndReviewaPrice', (req, res) => {
+    ListingsAndReview.find({ $and: [{ "price": { $gte: 80 } }, { "price": { $lte: 5000 } }] })
+        .exec((err, listingsAndReviews) => {
+            if (err) {
+                return res.status(400).json({
+                    ok: false,
+                    err
+                });
+            }
+            return res.status(200).json({
+                ok: true,
+                count: listingsAndReviews.length,
+                listingsAndReviews
+            });
+        });
+});
+
+app.get('/listingsAndReview/:property', (req, res) => {
+    let property = req.params.property
+    ListingsAndReview.find({ property_type: property })
+        .exec((err, listingsAndReviews) => {
+            if (err) {
+                return res.status(400).json({
+                    ok: false,
+                    err
+                });
+            }
+            return res.status(200).json({
+                ok: true,
+                count: listingsAndReviews.length,
+                listingsAndReviews
+            });
+        });
+});
+
 app.post('/listingsAndReview', (req, res) => {
     let body = req.body;
     let listingsAndReview = new ListingsAndReview({
